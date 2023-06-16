6f89bb05f409aa535568f1d0303c817dc6c6651a
Willybite <will.guinand@gmail.com>
2023-06-15 22:16:06 -0300

    password reset implementado

--- pet_shop/cuentas/urls.py
+++ pet_shop/cuentas/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 urlpatterns = [
     path("login/", views.inicioSesion, name="login"),
     path("logout/", views.salir, name="exit"),
@@ -7,4 +8,9 @@ urlpatterns = [
     path('perfil/', views.perfil, name='perfil'),
     path('editarUsuario/', views.editarUsuario, name='editarUsuario'),
     path('editarPerfil', views.editarPerfil, name='editarPerfil'),
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="cuentas/password-reset.html"), name='password_reset'),
+    path('reset_password_send/', auth_views.PasswordResetDoneView.as_view(template_name='cuentas/password-reset-send.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name='cuentas/password-reset-confirm.html'), name='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='cuentas/password-reset-completed.html'), name='password_reset_complete'),
+    
 ]
